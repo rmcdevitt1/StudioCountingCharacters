400f1ec37ffe63bdc6e034403dca175bbbee83cd
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-07-21 20:52:15 -0500

    finished

--- StudioCountingCharacters/Program.cs
+++ StudioCountingCharacters/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StudioCountingCharacters
 {
@@ -6,7 +7,36 @@ namespace StudioCountingCharacters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string myString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            char[] charactersInString = myString.ToCharArray();
+
+
+            //loop thru character array
+            Dictionary<char, int> charDictionary = new Dictionary<char, int>();
+            for (int i = 0; i < charactersInString.Length; i++)
+            {
+                //store and update character count
+                
+
+                //add character to dictionary
+                if (charDictionary.ContainsKey(charactersInString[i]))
+                {
+                    charDictionary[charactersInString[i]] += 1;
+                }
+                else
+                {
+                    charDictionary.Add(charactersInString[i], 1);
+                }
+            }
+
+                
+                foreach (KeyValuePair<char, int> character in charDictionary)
+                {
+                    Console.WriteLine(character.Key + " (" + character.Value + ")");
+                    
+                }
+
+            //print results
         }
     }
 }
